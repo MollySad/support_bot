655adccc35038e0830d922d49a47eb909a678502
MollySad <lane3154@gmail.com>
2024-12-01 20:15:50 +0700

    "Он мне ответил!"

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ async def urban_message(message):
 
 @dp.message_handler(commands=['start'])
 async def start(message):
-    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здорвью')
 
 
 @dp.message_handler()
 async def all_message(message):
-    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start чтобы начать общение')
 
 
 if __name__ == '__main__':
